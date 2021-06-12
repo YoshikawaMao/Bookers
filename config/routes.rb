cdaef3ba2eb752eff517b3434479f35b373bad41
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-12 10:53:47 +0000

    [add]homes,booksのコントローラー作成とviewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/new'
+  get 'homes/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
