1c8b965f691520dba509d031f6edf8b0c647f0e3
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-15 07:57:39 +0000

    [add]レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def  create
     @book = Book.new(book_params)
     @book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -19,9 +19,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
